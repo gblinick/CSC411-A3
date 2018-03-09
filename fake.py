4e39090377b2d64e69bd65086660b4c06c07a8c5
Ariel Kelman <ariel.kelman@mail.utoronto.ca>
2018-03-09 08:11:51 -0500

    first commit

---
+++ fake.py
@@ -0,0 +1,12 @@
+import os
+
+import numpy as np
+from numpy import random as rd
+import matplotlib.pyplot as plt
+
+os.chdir('/Users/arielkelman/Documents/Ariel/EngSci3-PhysicsOption/Winter2018/CSC411 - Machine Learning/Project2/CSC411/')
+
+
+
+
+if __name__ == "__main__":
