8af35924119f9814fc5ece532f8e78aa5e572fe3
gideon blinick <gideon.blinick@gmail.com>
2018-03-14 21:58:09 -0400

    adjusted part1

--- fake.py
+++ fake.py
@@ -34,21 +34,6 @@ def get_stats(lines):
     dict = { word:dict[word]/k for word in dict.keys() }
     return dict
 
-def get_numbers(lines):
-    ''' return a dictionary, where every word that appears in a headline in lines
-    is a key, with value equal to count(word)'''
-    dict = {}
-    for line in lines:
-        words = list(set( line.split(' ') )) #converting to set and back to a list removes duplicates
-        for word in words:
-            if word in dict.keys():
-                dict[word] += 1
-            else: 
-                dict[word] = 1
-    return dict
-
-
-
 def top_keywords(dict, num):
     ''' returns a list of tuples with the keys in dict having the highest value;
     num tuples will in the list'''
@@ -58,35 +43,12 @@ def top_keywords(dict, num):
     return kv
 
 
-
 if __name__ == "__main__":
     
     # Part 1
     
-    #with open('resources/clean_fake.txt') as f:
-    #    passage_fake = f.read()
-    
-    #words_fake = re.findall(r'\w+', passage_fake)
-    #cap_words_fake = [word.upper() for word in words_fake]
-    #word_counts_fake = Counter(cap_words_fake)
-    #most_common_fake = word_counts_fake.most_common(5)
-    
-    #with open('resources/clean_real.txt') as f:
-    #    passage_real = f.read()
-    
-    #words_real = re.findall(r'\w+', passage_real)
-    #cap_words_real = [word.upper() for word in words_real]
-    #word_counts_real = Counter(cap_words_real)
-    #most_common_real = word_counts_real.most_common(5)
-    
-    # part below almost right - have to get it so doesn't change the Counters themselves
-    #diff1 = word_counts_fake.subtract(word_counts_real)
-    #diff2 = word_counts_real.subtract(word_counts_fake)
-    
-    
     
     ## Get data
-    '''This is what Ariel stupidly did (in addition to the functions above)'''
     fake_lines = get_data('resources/clean_fake.txt') #Get list containing headlines
     real_lines = get_data('resources/clean_real.txt')
     
@@ -94,9 +56,14 @@ if __name__ == "__main__":
     ### By percentages
     fake_stats = get_stats(fake_lines) #compute probabilities for each word
     real_stats = get_stats(real_lines)
-    ### By counts
-    fake_counts = get_numbers(fake_lines)
-    real_counts = get_numbers(real_lines)
+   
+
+    new_real = {x: 0 for x in fake_stats if x not in real_stats}
+    new_fake = {x: 0 for x in real_stats if x not in fake_stats}
+    
+    fake_stats.update(new_fake)
+    real_stats.update(new_real)
+    
 
     ## Percentages
     ### Get top 10 
@@ -111,13 +78,7 @@ if __name__ == "__main__":
     fake_minus_real_keywords = top_keywords(fake_minus_real_perc, 10)
     real_minus_fake_keywords = top_keywords(real_minus_fake_perc, 10)
     
-    ## Counts
-    ### Get words that appear most often in one data set but don't appear in the other
-    not_in_real = {x: fake_counts[x] for x in fake_counts if x not in real_counts}
-    not_in_fake = {x: real_counts[x] for x in real_counts if x not in fake_counts}
-    ### Top counts
-    not_in_real_top = top_keywords(not_in_real, 10)
-    not_in_fake_top = top_keywords(not_in_fake, 10)
+    
     
     
     
